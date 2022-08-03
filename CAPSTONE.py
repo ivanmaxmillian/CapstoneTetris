cd81d5b4031d3419deb57eef30896fae3b801e80
Ivan Maxmillian Putra <69803751+ivanmaxmillian@users.noreply.github.com>
2022-08-03 21:24:26 +0700

    Update CAPSTONE.py

--- CAPSTONE.py
+++ CAPSTONE.py
@@ -8,8 +8,7 @@ from PIL import Image
 st.set_page_config(layout="centered")
 # End of Dashboard setup
 
-data1 = requests.get('https://gitlab.com/ivanmaxmillian/CapstoneTetris/-/raw/main/Data%20Bencana.xlsx')
-data_banjir = pd.read_excel(data1.content)
+data_banjir = pd.read_excel('C:/Users/ivanm/Documents/DOCUMENTS/BELAJAR/TETRIS/Project/Data Bencana.xlsx')
 data_banjir['Penyebab'].fillna(data_banjir['Kronologi & Dokumentasi'], inplace=True)
 
 st.title('Melihat Lebih Luas Banjir Indonesia')
